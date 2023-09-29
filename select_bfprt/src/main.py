5f4847629106e68404ca1a0f3c2d031f987877d1
Izaias Machado <izaiasmachado.dev@gmail.com>
2023-09-29 10:40:40 -0300

    feat: initial screens implementation

--- select_bfprt/src/main.py
+++ select_bfprt/src/main.py
@@ -19,12 +19,50 @@ def create_dataset_group():
     dataset_group = DatasetGroup(dataset_group_path)
     return dataset_group
 
-if __name__ == "__main__":
-    # for _ in range(1000):
-    #     create_instance()
-
+def execute_instances():
     algorithm_collection = create_algorithm_collection()
     dataset_group = create_dataset_group()
     
     executor = SelectBFPRTInstanceExecutor(algorithm_collection, dataset_group)
     executor.execute()
+
+def create_instances():
+    instances_amount = int(input("> Digite a quantidade de instâncias que deseja criar: "))
+
+    for _ in range(instances_amount):
+        create_instance()
+
+def clear_instances():
+    instances_folder_path = get_instances_folder_path()
+    instances_amount = len(os.listdir(instances_folder_path))
+
+    flag_delete = input(f"> Tem certeza que deseja deletar {format_number(instances_amount)} instâncias? [S/n]: ")
+    should_delete = flag_delete == "S" or flag_delete == "s"
+
+    if should_delete:
+        os.system(f"rm -rf {instances_folder_path}/*")
+        print("Instâncias deletadas com sucesso!")
+
+def main_menu():
+    while True:
+        print("===== Select BFPRT =====")
+        print("1 - Executar algoritmos para as Instâncias")
+        print("2 - Crias Instâncias")
+        print("3 - Limpar Instâncias")
+        print("0 - Sair")
+
+        option = int(input("> Opção: "))
+
+        if option == 1:
+            execute_instances()
+        elif option == 2:
+            create_instances()
+        elif option == 3:
+            clear_instances()
+        elif option == 0:
+            break
+        else:
+            print("[ERRO] Opção Inválida!")
+
+if __name__ == "__main__":
+    main_menu()
